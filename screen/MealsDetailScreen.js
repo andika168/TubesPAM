c83ec75482f83d7af23351623bdb237e1e9e0ca9
andika168 <andikaharispratama03@gmail.com>
2021-05-21 01:21:55 +0700

    -

--- screen/MealsDetailScreen.js
+++ screen/MealsDetailScreen.js
@@ -1,12 +1,33 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, Platform} from 'react-native'
+import HeaderButton from '../component/HeaderButton'
+import {HeaderButtons, Item} from 'react-navigation-header-buttons'
+import {DrawerActions} from '@react-navigation/native'
+import Color from '../constant/Color'
 
 const MealsDetailScreen = (props) =>{
     return(
         <View>
-            <Text>Ini adalah Favorite Screen</Text>
+            <Text>Ini adalah Detail Screen</Text>
         </View>
     )
 }
 
+export const MealsDetailOption = (navData) =>{
+    return {
+        headerTitle: "Food Recipes",
+        headerStyle: { 
+            backgroundColor: Platform.OS === "android" ? Color.primaryColor: "",
+        },
+        headerTintColor: Platform.OS === "android" ? "white" : Color.primaryColor,
+
+        headerTitleStyle: {
+            //
+        },
+        headerBackTitleStyle: {
+            //
+        },
+    }
+
+}
 export default MealsDetailScreen
