9559e8bb3b8e6f60f09cbf23e8fa443dbf937c7b
andika168 <andikaharispratama03@gmail.com>
2021-05-20 17:40:30 +0700

    membuat halaman detail makanan

---
+++ screen/MealsDetailScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const MealsDetailScreen = (props) =>{
+    return(
+        <View>
+            <Text>Ini adalah Favorite Screen</Text>
+        </View>
+    )
+}
+
+export default MealsDetailScreen
