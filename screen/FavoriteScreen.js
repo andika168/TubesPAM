ba0c5c4eea715b7e0e3b719f4d22db94f2439c3a
andika168 <andikaharispratama03@gmail.com>
2021-05-20 07:54:05 +0700

    Menambahkan file FavoriteScreen

---
+++ screen/FavoriteScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const FavoriteScreen = (props) =>{
+    return(
+        <View>
+            <Text>Ini adalah Favorite Screen</Text>
+        </View>
+    )
+}
+
+export default FavoriteScreen
